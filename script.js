0955b5183a063ed2ba3f8f002936c537ea13cacf
umer20i0784 <72217751+umer20i0784@users.noreply.github.com>
2024-12-13 23:23:41 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", async function () {
 
-    const adminPassword = "1";
+    const adminPassword = "admin";
     
     const employees = {
         "elisabeth": "Elisabeth Salas",
